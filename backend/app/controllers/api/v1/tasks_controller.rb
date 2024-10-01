157e126ac30656afc4561ba9aa2627051a1b1246
empitsu <oneok0813@gmail.com>
2024-10-01 22:45:55 +0900

    update

---
+++ backend/app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class TasksController < ApplicationController
+      def index
+        if Task.count.zero?
+          ["1st task", "2nd task", "3rd task"].each do |title|
+            Task.create!(title:title)
+          end
+        end
+
+        render json: {tasks: Task.all}
+      end
+    end
+  end
+end
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const subTitle = 'Zenn is good service!!';
+	let tasks: string[] = [];
+
+	async function getSomething() {
+		fetch('http://localhost:3000/api/v1/tasks')
+			.then((response) => response.json())
+			.then((data) => {
+				tasks = data.tasks;
+			});
+	}
+</script>
+
+<div class="container">
+	<div>
+		<h1 class="title">Zenn-app</h1>
+		<h2 class="subtitle">
+			{subTitle}
+		</h2>
+		<button on:click={getSomething}> タスク取得 </button>
+		<ul>
+			{#each tasks as task}
+				<li>{task.title}</li>
+			{/each}
+		</ul>
+	</div>
+</div>
